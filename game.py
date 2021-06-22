e9921583834de87344b5de9280866f9cf55066f7
Travis Milner <travismilner1@gmail.com>
2021-06-22 14:47:02 -0500

    refactored where user can input a number to be compared to computer randomized choice. Also separated my test assertions.

--- game.py
+++ game.py
@@ -1,12 +1,26 @@
-def hi_low(player, computer):
-    if player == computer:
-        return "Tie"
+import random
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+player = int(input(f"Choose a number: {numbers}"))
+computer = random.choice(numbers)
+
+def hi_low(p, c):
     
-    if player > computer:
-        return "Winner"
+    
+    if p == c:
+        print(f"Tie: {p} equals {c}")
+        return p and c
+    
+    if p > c:
+        print(f"Winner: {p} is better than {c}")
+        return p
 
-    if computer > player:
-        return "Loser"
+    if c > p:
+        print(f"Loser: {c} is greater than {p}")
+        return c
 
-        
+
+hi_low(player, computer)
+
 
--- test_main.py
+++ test_main.py
@@ -3,11 +3,21 @@ from game import hi_low
 import unittest
 
 class TestWin(unittest.TestCase):
-    def test_won(self):
+    def test_player_choice_wins_if_higher(self):
         
-        self.assertEqual("Loser",hi_low(5,10))
-        self.assertEqual("Tie", hi_low(10,10))
-        self.assertEqual("Winner", hi_low(10,5))
+        
+        actual = hi_low(10, 5)
+        self.assertEqual(10,actual)
+
+    def test_computer_choice_wins_if_higher(self):
+         
+         actual = hi_low(2,7)
+         self.assertEqual(7,actual)
+
+    def test_computer_choice_player_choice_same_is_tie(self):
+        
+        actual = hi_low(5,5)
+        self.assertEqual(5,actual)
 
 if __name__ == '__main__':
     unittest.main()
